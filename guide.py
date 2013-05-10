db03e33f4284305ec03b71eb982975931f065eed
xiongchiamiov <xiong.chiamiov@gmail.com>
2013-05-09 21:40:13 -0700

    Add more guide attributes.
    But not all of the ones available to us, because some will need more advanced
    data types that I don't want to deal with yet.
    
    Also, remove the static list of attributes, because there are a lot of them and
    I don't want to maintain two lists.  dir(guide) will just have to suffer.

--- guide.py
+++ guide.py
@@ -6,23 +6,46 @@ from category import Category
 API_BASE_URL = 'https://www.ifixit.com/api/1.1'
 
 class Guide(object):
-   _attributes = ['category']
-   
    def __init__(self, guideid):
       self.id = guideid
       
-   def __dir__(self):
-      return dir(super(Guide, self)) + self._attributes
-   
    def __getattr__(self, name):
-      if name not in self._attributes:
-         raise AttributeError("'Guide' object has no attribute '%s'" % name)
       self.refresh()
-      return self.__dict__[name]
+      try:
+         return self.__dict__[name]
+      except KeyError:
+         raise AttributeError("'Guide' object has no attribute '%s'" % name)
    
    def refresh(self):
       response = requests.get('%s/guides/%s' % (API_BASE_URL, self.id))
       attributes = response.json()
       
       self.category = Category(attributes['category'])
+      self.url = attributes['url']
+      
+      guide = attributes['guide']
+      self.title = guide['title']
+      #self.image = guide['image']
+      self.locale = guide['locale']
+      #self.introduction = WikiText(guide['introduction_raw'],
+      #                             guide['introduction_rendered'])
+      #self.conclusion = WikiText(guide['conclusion_raw'],
+      #                           guide['conclusion_rendered'])
+      #self.tools = guide['tools']
+      #self.parts = guide['parts']
+      self.subject = guide['subject']
+      #self.modifiedDate = guide['modified_date']
+      #self.documents = guide['documents']
+      author = guide['author']
+      #self.author = User(author['userid'], name=author['text'])
+      #self.timeRequired = guide['timeRequired']
+      #self.steps = guide['steps']
+      self.type = guide['type']
+      self.public = guide['public']
+      self.revision = guide['revisionid']
+      self.difficulty = guide['difficulty']
+      #self.prerequisites = guide['prerequisites'],
+      #                     guide['prereq_modified_date']
+      #self.summary = guide['summary']
+      #self.flags = guide['flags']
    
