567b175d5a070c426ac994fd572211d42a19186d
xiongchiamiov <xiong.chiamiov@gmail.com>
2013-09-08 10:12:53 -0700

    Add invoke task for serving documentation.
    Less typing, and a bit more obvious for anyone else.

--- tasks.py
+++ tasks.py
@@ -9,6 +9,11 @@ def doc():
    run('cd docs && make html')
 
 @task
+def docserve():
+   print 'Serving docs on localhost:8000...'
+   run('cd docs/_build/html && python -m SimpleHTTPServer')
+
+@task
 def publish():
    run('./setup.py sdist upload')
 
